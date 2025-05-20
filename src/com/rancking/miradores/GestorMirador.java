702540cbcc799ae4ed126767a4a024041f3bc51f
educole3335 <usr3335@salesianos-lacuesta.net>
2025-05-20 09:58:02 +0100

    feat(GestorMirador.java):añadimos la arraylist con sus metodos añadir, ordenar, eliminar, gety total

--- src/com/rancking/miradores/GestorMirador.java
+++ src/com/rancking/miradores/GestorMirador.java
@@ -1,5 +1,32 @@
 package com.rancking.miradores;
+import java.util.*;
 
 public class GestorMirador {
-    
+    private static final List<Mirador> lista = new ArrayList<>();
+
+    public static void añadir(Mirador m) {
+        lista.add(m);
+    }
+
+    public static List<Mirador> getTodosOrdenados() {
+        lista.sort(Comparator.comparingInt(Mirador::getPuntuacion).reversed());
+        return lista;
+    }
+
+    public static boolean eliminar(int index) {
+        if (index >= 0 && index < lista.size()) {
+            lista.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public static Mirador get(int index) {
+        return lista.get(index);
+    }
+
+    public static int total() {
+        return lista.size();
+    }
 }
+
